fb1d3fbf9e85b4b54cf33f53db8b7b18b1e3ba63
Leonardo Sabino <leonardo.sabino.s@hotmail.com>
2021-11-15 11:14:23 -0300

    code improvement

--- scene/01/scene_01.gd
+++ scene/01/scene_01.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _input(event):
 	if event.is_action_pressed("interact"):
 		var dialogue_player = get_node_or_null("dialog")
-		dialogue_player.play()
+		dialogue_player.play("res://shared/dialog/json/dialog.json") #TODO: importar json de forma estática
--- shared/dialog/dialog.gd
+++ shared/dialog/dialog.gd
@@ -1,18 +1,16 @@
 extends CanvasLayer
 
-var path = "res://shared/dialog/json/dialog.json"
-
 var dialogues = []
 var current_dialogue_id = 0
 
 func _ready():
 	$NinePatchRect.visible = false
 
-func play():
+func play(json):
 	var player = get_tree().get_root().find_node('player', true, false)
 	player._set_active(false)
 	$NinePatchRect.visible = true
-	dialogues = load_dialogue()
+	dialogues = load_dialogue(json)
 	
 	current_dialogue_id = -1
 	next_line()
@@ -34,8 +32,8 @@ func next_line():
 	$NinePatchRect/name.text = dialogues[current_dialogue_id]['name']
 	$NinePatchRect/message.text = dialogues[current_dialogue_id]['text']
 	
-func load_dialogue():
+func load_dialogue(json):
 	var file = File.new()
-	if file.file_exists(path):
-		file.open(path, file.READ)
+	if file.file_exists(json):
+		file.open(json, file.READ)
 		return parse_json(file.get_as_text())
