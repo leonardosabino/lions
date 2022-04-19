83a210e1817c293e305bb4def43a0764ca299baf
Leonardo Sabino <leonardo.sabino.s@hotmail.com>
2022-04-18 21:57:28 -0300

    refactor dialog system

--- scene/01/scene_01.gd
+++ scene/01/scene_01.gd
@@ -2,12 +2,6 @@ extends Node2D
 
 
 func _input(event):
-	if event.is_action_pressed("interact"):
-		var player = get_tree().get_root().find_node('player_identity', true, false)
-		var dialogue_player = get_node_or_null("dialog")
-		if player.playerName == "leo":
-			dialogue_player.play("res://shared/dialog/json/dialog-leo.json")
-		if player.playerName == "joao":
-			dialogue_player.play("res://shared/dialog/json/dialog-joao.json")
-		if player.playerName == "natalia":
-			dialogue_player.play("res://shared/dialog/json/dialog-naty.json")
+	if event.is_action_pressed("interact"):		
+		var new_dialog = Dialogic.start('Scene01-first') 
+		add_child(new_dialog)
